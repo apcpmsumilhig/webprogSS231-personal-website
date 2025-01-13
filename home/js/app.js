5ed985df524a0613387a2d2d0ef3e4f7c07ed88a
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-01-13 23:32:12 +0800

    fix

--- home/js/app.js
+++ home/js/app.js
@@ -4,11 +4,11 @@ const app = Vue.createApp({
        manyFoods: [
          {name: 'Friends', url: 'gallery/css/assets/1.JPG'},
          {name: 'Cover', url: 'gallery/css/assets/5.jpg.svg'},
-         {name: 'Sun', url: '7.jpg'},
+         {name: 'Sun', url: 'gallery/css/assets/7.jpg'},
          {name: 'Plant', url: 'gallery/css/assets/8.jpg'},
          {name: 'Me', url: 'gallery/css/assets/9.jpg'},
-         {name: 'Wife', url: 'profile.jpeg'},
-         {name: 'Cat', url: '11.png'}
+         {name: 'Wife', url: 'gallery/css/assets/profile.jpeg'},
+         {name: 'Cat', url: 'gallery/css/assets/11.png'}
        ]
      }
     }
