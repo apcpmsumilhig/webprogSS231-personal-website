4a7b295a68f218a728914d262d5a368cb6152021
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-01-13 23:28:55 +0800

    check

---
+++ home/js/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+     return {
+       manyFoods: [
+         {name: 'Friends', url: 'gallery/css/assets/1.JPG'},
+         {name: 'Cover', url: 'gallery/css/assets/5.jpg.svg'},
+         {name: 'Sun', url: '7.jpg'},
+         {name: 'Plant', url: 'gallery/css/assets/8.jpg'},
+         {name: 'Me', url: 'gallery/css/assets/9.jpg'},
+         {name: 'Wife', url: 'profile.jpeg'},
+         {name: 'Cat', url: '11.png'}
+       ]
+     }
+    }
+   })
+   
+   app.mount('#app')
